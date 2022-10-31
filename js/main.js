6d0958095814924c3c0bba4266173de3258959d6
Daniel Cho <chodaniel89@gmail.com>
2022-10-31 23:07:46 +0000

    Finished submit function.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-var photoInput = document.querySelector('#photo-url');
+var photoInput = document.querySelector('#photourl');
 var photo = document.querySelector('.photo');
+var photoDefault = 'images/placeholder-image-square.jpg';
 
 function handlePhoto(event) {
   photo.setAttribute('src', event.target.value);
 }
 
 photoInput.addEventListener('input', handlePhoto);
+
+var entryLog = document.querySelector('#entry');
+
+function submitEntry(event) {
+  event.preventDefault();
+  var entryData = {};
+  entryData.title = entryLog.elements.title.value;
+  entryData.photourl = entryLog.elements.photourl.value;
+  entryData.notes = entryLog.elements.notes.value;
+  entryData.nextEntryID = data.nextEntryID;
+  data.nextEntryId++;
+  data.entries = entryData;
+  photo.setAttribute('src', photoDefault);
+  entryLog.reset();
+
+}
+
+entryLog.addEventListener('submit', submitEntry);
