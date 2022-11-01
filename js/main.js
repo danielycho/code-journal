f9733b919f7502ee0b73d95560fc01fda4bae3e7
Daniel Cho <chodaniel89@gmail.com>
2022-11-01 03:58:30 +0000

    line 21 issue still not resolved.

--- js/main.js
+++ js/main.js
@@ -12,17 +12,30 @@ var entryLog = document.querySelector('#entry');
 
 function submitEntry(event) {
   event.preventDefault();
-  var entryData = {};
-  entryData.title = entryLog.elements.title.value;
-  entryData.photourl = entryLog.elements.photourl.value;
-  entryData.notes = entryLog.elements.notes.value;
-  entryData.EntryID = 0;
-  entryData.EntryID++;
-  data.nextEntryId++;
-  data.entries.push(entryData);
+  var singleData = {
+    title: $title.value,
+    photoURL: $imageLink.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId++
+  };
+  data.entries.unshift(singleData);
+  // var entryData = {};
+  // entryData.title = entryLog.elements.title.value;
+  // entryData.photourl = entryLog.elements.photourl.value;
+  // entryData.notes = entryLog.elements.notes.value;
+  // entryData.entryId = data.nextEntryId;
+  // console.log(typeof entryData, typeof data.entries);
+  // console.log(entryData);
+  // data.entries.unshift(entryData);
+  // data.nextEntryId++;
   photo.setAttribute('src', photoDefault);
   entryLog.reset();
 
 }
 
 entryLog.addEventListener('submit', submitEntry);
+
+var $imageLink = document.querySelector('input[name="photourl"]');
+var $notes = document.querySelector('textarea[name = "notes"]');
+
+var $title = document.querySelector('input[name="title"]');
