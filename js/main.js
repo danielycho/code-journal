5bc33c0b13a68d8b222843b7891935650967bfd8
Daniel Cho <chodaniel89@gmail.com>
2022-11-01 00:00:11 +0000

    Need to work on feature 1.

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ function submitEntry(event) {
   entryData.title = entryLog.elements.title.value;
   entryData.photourl = entryLog.elements.photourl.value;
   entryData.notes = entryLog.elements.notes.value;
-  entryData.nextEntryID = data.nextEntryID;
+  entryData.EntryID = 0;
+  entryData.EntryID++;
   data.nextEntryId++;
   data.entries.push(entryData);
   photo.setAttribute('src', photoDefault);
